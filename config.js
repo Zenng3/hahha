8993df7f1beecea6fda1ad5b430b0cb0551316d7
Zenng3 <85269748+Zenng3@users.noreply.github.com>
2021-06-03 12:48:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,18 @@
 const CONFIG = {
-    introTitle: 'cái ghế à!',
-    introDesc: `dù 1/6 hay còn gọi là quốc tế thiếu nhi đã qua nhưng m có muốn tặng quà không hãy nói cho t biết`,
-    btnIntro: 'ok ',
-    title: 'phải chăng mày không thích t tặng quà đúng không',
-    desc: 'Phải chăng m không thích làm thiếu nhi nữa đúng không ',
-    btnYes: 'ừ t muốn m không phải mất tiền ',
-    btnNo: 'có chứ t vẫn muốn làm thiếu nhi',
-    question:'Trên thế giới hơn 7 tỉ người mà sao m lại đòi quà t (mà quên dân số tăng lên 8 tỉ rồi) <3',
-    btnReply: 'cảm ơn vì đã tiết kiệm tiền cho t',
-    reply: 'cảm ơn vì đã tiết kiệm tiền cho t :)',
-    mess: 't biết mà 🥰. cảm ơn m nhiều nhiều nhiều 😘😘',
-    messDesc: 'hỏi lại lần cuối nhé m có thích quà không.',
-    btnAccept: 'khôngggg',
-    
+    introTitle: 'mày à!',
+    introDesc: `dù qua 1/6 hay còn gọi là
+    quốc tế thiếu nhi á ,mày có muốn tặng 
+    quà không hãy trả lời cho tao biết`,
+    btnIntro: 'ok',
+    title: 'Phải chăng mày không muốn tặng quà 😙',
+    desc: 'Phải chăng mày đã trưởng thành ',
+    btnYes: 'đúng rồi tao đã trưởng thành',
+    btnNo: 'không t vẫn muốn tặng quà',
+    question:'Trên thế giới hơn 7 tỉ người mà mày lại đòi quà t',
+    btnReply: 'gửi cho tao',
+    reply: 'thôi t không cần quà nữa đâu',
+    mess: 'tao biết mà cảm ơn mày nhiều',
+    messDesc: 'vày là m không cần quà đúng không',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.messenger.com/t/100057340044692'
 }
